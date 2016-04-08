7884e51d462fe2163e9678f023811798fd527b1b
Peter Jang <peter.x.jang@gmail.com>
2016-04-07 21:32:00 -0500

    moved api base url to environment variable

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -16,7 +16,7 @@ class Employee
 
   def update(input_options)
     employee_hash = Unirest.patch(
-      "http://localhost:3000/api/v1/employees/#{id}.json",
+      "#{ENV['API_BASE_URL']}/employees/#{id}.json",
       headers: { "Accept" => "application/json" },
       parameters: input_options
     ).body
@@ -24,11 +24,11 @@ class Employee
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v1/employees/#{id}.json")
+    Unirest.delete("#{ENV['API_BASE_URL']}/employees/#{id}.json")
   end
 
   def self.all
-    employee_hashes = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+    employee_hashes = Unirest.get("#{ENV['API_BASE_URL']}/employees.json").body
     employees = []
     employee_hashes.each do |employee_hash|
       employees << Employee.new(employee_hash)
@@ -38,13 +38,13 @@ class Employee
 
   def self.find_by(input_options)
     id = input_options[:id]
-    employee_hash = Unirest.get("http://localhost:3000/api/v1/employees/#{id}.json").body
+    employee_hash = Unirest.get("#{ENV['API_BASE_URL']}/employees/#{id}.json").body
     Employee.new(employee_hash)
   end
 
   def self.create(input_options)
     employee_hash = Unirest.post(
-      "http://localhost:3000/api/v1/employees",
+      "#{ENV['API_BASE_URL']}/employees",
       headers: { "Accept" => "application/json" },
       parameters: input_options
     ).body
